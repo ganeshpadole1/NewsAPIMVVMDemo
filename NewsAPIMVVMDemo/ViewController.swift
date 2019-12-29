8c0541fe7a4e58704c58e810000b527cfed623e7
ganesh padole <ganeshpadole55@gmail.com>
2019-12-29 19:40:46 +0530

    Initial Commit

---
+++ NewsAPIMVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsAPIMVVMDemo
+//
+//  Created by ganesh padole on 29/12/19.
+//  Copyright © 2019 ganesh padole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
